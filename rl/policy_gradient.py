Fixed policy gradient

rl/policy_gradient.py
@@ -16,19 +16,18 @@ class PolicyGradient(BaseAgent):
         #    Hyper parameters  #
         # ==================== #
         self.learning_rate = 1e-1
-        self.gamma = .99
 
         # ==================== #
         #        Memory        #
         # ==================== #
         self.batch_size = 10
-        self.obs, self.acts, self.rews = [], [], []
+        self.episode_rewards = []
         self.b_obs, self.b_acts, self.b_rews = [], [], []
 
         # ==================== #
         #        Network       #
         # ==================== #
-        self.states, self.actions, self.advantages, self.output = self.create_network()
+        self.states, self.actions, self.advantages, self.output, self.logits = self.create_network()
         self.optimiser = self.loss_fn()
 
         self.session = tf.Session()
@@ -37,10 +36,10 @@ class PolicyGradient(BaseAgent):
 
     def create_network(self):
         states = tf.placeholder(np.float32, shape=[None, self.state_size], name='input')
-        actions = tf.placeholder(np.float32, shape=[None, self.num_actions], name='q_values_new')
-        advantages = tf.placeholder(np.float32, shape=[None], name='rewards')
+        actions = tf.placeholder(np.int32)
+        advantages = tf.placeholder(np.float32)
 
-        hidden1 = tf.contrib.layers.fully_connected(
+        hidden = tf.contrib.layers.fully_connected(
             inputs=states,
             num_outputs=36,
             activation_fn=tf.nn.relu,
@@ -48,18 +47,21 @@ class PolicyGradient(BaseAgent):
         )
 
         logits = tf.contrib.layers.fully_connected(
-            inputs=hidden1,
+            inputs=hidden,
             num_outputs=self.num_actions,
-            activation_fn=tf.nn.softmax
+            activation_fn=None
         )
+        _sample = tf.reshape(tf.multinomial(logits, 1), [])
 
-        # sample = tf.reshape(tf.multinomial(logits, 1), [])
-
-        return states, actions, advantages, logits
+        return states, actions, advantages, _sample, logits
 
     def loss_fn(self):
-        loss = tf.log(tf.reduce_sum(tf.multiply(self.actions, self.output))) * self.advantages
-        return tf.train.AdamOptimizer(self.learning_rate).minimize(-loss)
+        log_prob = tf.log(tf.nn.softmax(self.logits))
+        indices = tf.range(0, tf.shape(log_prob)[0]) * tf.shape(log_prob)[1] + self.actions
+        act_prob = tf.gather(tf.reshape(log_prob, [-1]), indices)
+        loss = -tf.reduce_sum(tf.multiply(act_prob, self.advantages))
+
+        return tf.train.AdamOptimizer(self.learning_rate).minimize(loss)
 
     def step(self, render=False):
         if render:
@@ -69,18 +71,15 @@ class PolicyGradient(BaseAgent):
 
         next_state, reward, done, _ = self.env.step(action)  # observe the results from the action
 
-        self.add(self.current_state, action, reward, done, next_state)
+        self.add(self.current_state, action, reward, None, None)
 
         self.current_state = next_state
 
         if done:
-
-            self.b_obs.extend(self.obs)
-            self.b_acts.extend(self.acts)
-            advantages = self.process_rewards(self.rews)
+            advantages = self.process_rewards(self.episode_rewards)
             self.b_rews.extend(advantages)
 
-            self.obs, self.acts, self.rews = [], [], []
+            self.episode_rewards = []
 
             if self.episode % self.batch_size == 0 and self.episode > 0:
                 self.train()
@@ -88,17 +87,16 @@ class PolicyGradient(BaseAgent):
         return reward, done
 
     def run(self, state):
-        return self.session.run(self.output, {self.states: [state]})[0]
+        return self.session.run(self.output, {self.states: [state]})
 
     def get_action(self, current_state):
-        output = self.run(current_state)
-        return np.argmax(np.random.multinomial(1, output))
+        return self.run(current_state)
 
     def train(self):
         print('== TRAINING ==')
         states, actions, ads = self.get()
         self.session.run(self.optimiser, feed_dict={self.states: states, self.actions: actions, self.advantages: ads})
-        self.delete()
+        self.clear_memory()
 
     def save(self):
         pass
@@ -107,16 +105,16 @@ class PolicyGradient(BaseAgent):
         pass
 
     def add(self, current_state, action, reward, done, next_state):
-        self.obs.append(current_state)
-        action = self.one_hot_encode(action)
-        self.acts.append(action)
-        self.rews.append(reward)
+        self.b_obs.append(self.current_state)
+        # action = self.one_hot_encode(action)
+        self.b_acts.append(action)
+        self.episode_rewards.append(reward)
 
     def get(self):
         self.b_rews = (self.b_rews - np.mean(self.b_rews)) // (np.std(self.b_rews) + 1e-10)
         return self.b_obs, self.b_acts, self.b_rews
 
-    def delete(self):
+    def clear_memory(self):
         self.b_obs, self.b_acts, self.b_rews = [], [], []
 
     @staticmethod